83610f624436c1c0f3c70360201da222032d9961
Jaeryong Im <hackerrs.im@gmail.com>
2019-04-13 21:53:36 +0900

    TapeEquilibrium Algorithm

---
+++ CodilityTraining/TapeEquilibrium.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CodilityTraining
+{
+    class TapeEquilibrium
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            int minimum = A[0];
+            int arraySum = A.Sum();
+            int eachSum = 0;
+
+            for (int p = 1; p < A.Length; p++)
+            {
+                eachSum += A[p - 1];
+                int value = arraySum - eachSum;
+                if (p == 1)
+                {
+                    minimum = Math.Abs(eachSum - value);
+                    continue;
+                }
+
+                int difference = Math.Abs(eachSum - value);
+                if (difference < minimum)
+                {
+                    minimum = difference;
+                }
+            }
+
+            return minimum;
+        }
+
+    }
+}
