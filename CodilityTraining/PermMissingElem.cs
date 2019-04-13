52430a8ebd90e0814c7936478e88dbb98ba14cbd
Jaeryong Im <hackerrs.im@gmail.com>
2019-04-13 21:50:05 +0900

    PermMissingElem Algorithm

---
+++ CodilityTraining/PermMissingElem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodilityTraining
+{
+    class PermMissingElem
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            int arrayLength = A.Length;
+            if (arrayLength == 0)
+            {
+                return 1;
+            }
+
+            Array.Sort(A);
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (A[i] == i + 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    return i + 1;
+                }
+            }
+
+            return A[arrayLength - 1] + 1;
+        }
+
+    }
+}
