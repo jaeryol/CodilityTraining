134f279c8c87ca34dbdbb98abc23639980a70fbc
Jaeryong Im <hackerrs.im@gmail.com>
2019-04-14 12:22:55 +0900

    PermCheck Algorithm

---
+++ CodilityTraining/PermCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodilityTraining
+{
+    class PermCheck
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            Array.Sort(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != i + 1)
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+
+    }
+}
